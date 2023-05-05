c9e37799305fb81aae00028077515c4cde1d0b1d
Rosana Veroneze <rveroneze@gmail.com>
2023-05-05 15:07:55 +0200

    executable of the pattern mining algorithms

---
+++ FSbAC/PM/runPM_RInClose_dataConcatFS.sh
@@ -0,0 +1,12 @@
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p0.PM cvcp ../data/folds/dataConcat.discr.minsups10.p0 1 0 dataConcatFSbAC.discr.CI.ms10.p0.py ../data/folds/dataConcat.labels.train.p0 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p1.PM cvcp ../data/folds/dataConcat.discr.minsups10.p1 1 0 dataConcatFSbAC.discr.CI.ms10.p1.py ../data/folds/dataConcat.labels.train.p1 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p2.PM cvcp ../data/folds/dataConcat.discr.minsups10.p2 1 0 dataConcatFSbAC.discr.CI.ms10.p2.py ../data/folds/dataConcat.labels.train.p2 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p3.PM cvcp ../data/folds/dataConcat.discr.minsups10.p3 1 0 dataConcatFSbAC.discr.CI.ms10.p3.py ../data/folds/dataConcat.labels.train.p3 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p4.PM cvcp ../data/folds/dataConcat.discr.minsups10.p4 1 0 dataConcatFSbAC.discr.CI.ms10.p4.py ../data/folds/dataConcat.labels.train.p4 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p5.PM cvcp ../data/folds/dataConcat.discr.minsups10.p5 1 0 dataConcatFSbAC.discr.CI.ms10.p5.py ../data/folds/dataConcat.labels.train.p5 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p6.PM cvcp ../data/folds/dataConcat.discr.minsups10.p6 1 0 dataConcatFSbAC.discr.CI.ms10.p6.py ../data/folds/dataConcat.labels.train.p6 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p7.PM cvcp ../data/folds/dataConcat.discr.minsups10.p7 1 0 dataConcatFSbAC.discr.CI.ms10.p7.py ../data/folds/dataConcat.labels.train.p7 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p8.PM cvcp ../data/folds/dataConcat.discr.minsups10.p8 1 0 dataConcatFSbAC.discr.CI.ms10.p8.py ../data/folds/dataConcat.labels.train.p8 1
+./RInClose_mminsup ../data/folds/dataConcatFSbAC.discr.train.p9.PM cvcp ../data/folds/dataConcat.discr.minsups10.p9 1 0 dataConcatFSbAC.discr.CI.ms10.p9.py ../data/folds/dataConcat.labels.train.p9 1
+
+
---
+++ FSbAC/PM/runPM_TalkyG_dataConcatFS.sh
@@ -0,0 +1,13 @@
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p0.PM 2 ../data/folds/dataConcat.discr.minsups10.p0 dataConcatFSbAC.discr.MG.ms10.p0.py ../data/folds/dataConcat.labels.train.p0 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p1.PM 2 ../data/folds/dataConcat.discr.minsups10.p1 dataConcatFSbAC.discr.MG.ms10.p1.py ../data/folds/dataConcat.labels.train.p1 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p2.PM 2 ../data/folds/dataConcat.discr.minsups10.p2 dataConcatFSbAC.discr.MG.ms10.p2.py ../data/folds/dataConcat.labels.train.p2 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p3.PM 2 ../data/folds/dataConcat.discr.minsups10.p3 dataConcatFSbAC.discr.MG.ms10.p3.py ../data/folds/dataConcat.labels.train.p3 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p4.PM 2 ../data/folds/dataConcat.discr.minsups10.p4 dataConcatFSbAC.discr.MG.ms10.p4.py ../data/folds/dataConcat.labels.train.p4 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p5.PM 2 ../data/folds/dataConcat.discr.minsups10.p5 dataConcatFSbAC.discr.MG.ms10.p5.py ../data/folds/dataConcat.labels.train.p5 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p6.PM 2 ../data/folds/dataConcat.discr.minsups10.p6 dataConcatFSbAC.discr.MG.ms10.p6.py ../data/folds/dataConcat.labels.train.p6 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p7.PM 2 ../data/folds/dataConcat.discr.minsups10.p7 dataConcatFSbAC.discr.MG.ms10.p7.py ../data/folds/dataConcat.labels.train.p7 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p8.PM 2 ../data/folds/dataConcat.discr.minsups10.p8 dataConcatFSbAC.discr.MG.ms10.p8.py ../data/folds/dataConcat.labels.train.p8 1
+./TalkyG_mminsup ../data/folds/dataConcatFSbAC.discr.train.p9.PM 2 ../data/folds/dataConcat.discr.minsups10.p9 dataConcatFSbAC.discr.MG.ms10.p9.py ../data/folds/dataConcat.labels.train.p9 1
+
+
+
